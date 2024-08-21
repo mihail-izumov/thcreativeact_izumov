7cc9f2e7508b9c981f1e365508059a922dd2e637
mihail-izumov <mihail.izyumov@gmail.com>
2024-08-21 21:21:49 +0400

    Quartz sync: Aug 21, 2024, 9:21 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,8 +13,8 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}> 
-        <p><b><a href="https://t.me/izumov" target="_blank">Телеграм-канал</a></b>      <b><a href="/credits" target="_blank">Информация о сайте</a></b>      <b><a href="mailto:izumov@thecreativeact.ru" target="_blank">Контакт</a></b></p>
-        <b><a href="/docs/oferta" target="_blank">Оферта</a></b> <b><a href="/docs/politic" target="_blank">Политика конфиденциальности</a></b>
+        <p><b><a href="https://t.me/izumov" target="_blank">Телеграм-канал   </a></b> <b><a href="/credits" target="_blank">Информация о сайте    </a></b> <b><a href="mailto:izumov@thecreativeact.ru" target="_blank">Контакт</a></b></p>
+        <p><b><a href="/docs/oferta" target="_blank">Оферта   </a></b> <b><a href="/docs/politic" target="_blank">Политика конфиденциальности</a></b></p>
         <b><a href="https://thecreativeact.ru" target="_blank">Чудесная Гостиная</a></b>
         <p>© 2024 Михаил Изюмов</p>
       </footer>
